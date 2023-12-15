42682b930bd6220b92925254c5a62b47bae9a082
Emanuel Costa <82065728+emanuelcop3@users.noreply.github.com>
2023-12-15 14:20:09 -0300

    Feat:prova-pratica

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # 20232BSET03P2
 Inteli - Engenharia de Software | Avaliação 2023-2B P2
+
+
+# Cats and Dogs Vote App
+
+Abaixo estão as vulnerabilidades identificadas e as medidas adotadas para corrigi-las:
+
+## Vulnerabilidades Identificadas e Medidas Corretivas
+
+1. **SQL Injection em Rotas de Inserção (`/cats` e `/dogs`):**
+   - **Descrição:** O código original utilizava interpolação de strings para inserção no banco de dados, o que tornava suscetível a ataques de SQL Injection.
+   - **Medida Corretiva:** As consultas SQL foram alteradas para utilizar prepared statements, garantindo assim a sanitização e validação dos dados de entrada.
+
+2. **Lógica de Votação sem Verificação de Existência do Registro:**
+   - **Descrição:** A lógica de votação não verificava se o registro do animal existia antes de adicionar um voto, o que poderia levar a operações inválidas.
+   - **Medida Corretiva:** Foi adicionada uma consulta prévia para verificar a existência do registro antes de adicionar um voto, evitando assim operações em registros inexistentes.
+
+3. **Tratamento de Erros Não Adequado:**
+   - **Descrição:** O tratamento de erros nas rotas estava incompleto e poderia expor detalhes de implementação no caso de falhas.
+   - **Medida Corretiva:** Foram implementados tratamentos de erro mais adequados, registrando mensagens genéricas no log e fornecendo respostas de erro ao cliente sem expor detalhes sensíveis.
+
+4. **Métodos Não Implementados:**
+   - **Descrição:** Algumas rotas e métodos declarados não estavam completamente implementados.
+   - **Medida Corretiva:** Foram implementados todos os métodos que possuíam assinatura no código, garantindo o funcionamento correto de todas as funcionalidades.
+
+
--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, votes INTEGER)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, votes INTEGER)");
 });
 
 app.post('/cats', (req, res) => {
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+  db.run("INSERT INTO cats (name, votes) VALUES (?, 0)", [name], function(err) {
     if (err) {
+      console.error(err.message);
       res.status(500).send("Erro ao inserir no banco de dados");
     } else {
       res.status(201).json({ id: this.lastID, name, votes: 0 });
@@ -27,18 +27,56 @@ app.post('/cats', (req, res) => {
 });
 
 app.post('/dogs', (req, res) => {
-  
+  const name = req.body.name;
+  db.run("INSERT INTO dogs (name, votes) VALUES (?, 0)", [name], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Erro ao inserir no banco de dados");
+    } else {
+      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
 });
 
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+  const animalType = req.params.animalType;
+  const id = req.params.id;
+
+  // Valide e sanitize dados de entrada para evitar SQL Injection
+  const validAnimalTypes = ['cats', 'dogs'];
+  if (!validAnimalTypes.includes(animalType)) {
+    res.status(400).send("Tipo de animal inválido");
+    return;
+  }
+
+  // Verifique se o registro do animal existe antes de adicionar um voto
+  const tableName = (animalType === 'cats') ? 'cats' : 'dogs';
+  const checkQuery = `SELECT * FROM ${tableName} WHERE id = ?`;
+
+  db.get(checkQuery, [id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Erro ao verificar o banco de dados");
+    } else if (!row) {
+      res.status(404).send("Registro do animal não encontrado");
+    } else {
+      // Utilize prepared statement para evitar SQL Injection
+      db.run(`UPDATE ${tableName} SET votes = votes + 1 WHERE id = ?`, [id], function(err) {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send("Erro ao atualizar o banco de dados");
+        } else {
+          res.status(200).send("Voto computado");
+        }
+      });
+    }
+  });
 });
 
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
+      console.error(err.message);
       res.status(500).send("Erro ao consultar o banco de dados");
     } else {
       res.json(rows);
@@ -47,12 +85,19 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Ocorreu um erro!');
+  res.status(500).send('Ocorreu um erro interno no servidor');
 });
 
 app.listen(port, () => {
